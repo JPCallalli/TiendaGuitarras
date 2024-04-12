13d2750f6d7114a778bcb1b8c383734bb1db387f
Jean Callalli <jeanpl13@hotmail.com>
2024-04-11 19:00:55 -0500

    agregando carrito

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,44 @@
+import { useState, useEffect } from "react";
 import Header from "./components/Header";
 import Guitar from "./components/Guitar";
+import { db } from "./data/db";
 
 function App() {
+
+  const [data, setData] = useState(db);
+  const [cart, setCart] = useState([]);
+
+  function addToCart (item) {
+
+    const itemExist = cart.findIndex(guitar => guitar.id === item.id)
+    if(itemExist >=0) {
+      const updateCart = [...cart]
+      updateCart[itemExist].quantity++
+      setCart(updateCart)
+    }else{
+      item.quantity = 1
+      setCart([...cart, item])
+    }
+  }
+
   return (
     <>
-
-    <Header />
+      <Header 
+        cart={cart}
+      />
 
       <main className="container-xl mt-5">
         <h2 className="text-center">Nuestra Colección</h2>
 
         <div className="row mt-5">
-          <Guitar />
+          {data.map((guitar) => (
+            <Guitar
+              key={guitar.id} 
+              guitar={guitar}
+              setCart={setCart}
+              addToCart={addToCart}
+            />
+          ))}
         </div>
       </main>
 
